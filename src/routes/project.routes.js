372620a71602e8c66853f76ca4d14777a7d717d9
Quốc Bảo <quocbaogg1407@gmail.com>
2026-07-22 09:08:11 +0700

    Cập nhật: Thêm Route cho API Project để sửa lỗi 404

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
 const boardRoutes = require("./routes/board.routes");
 const taskRoutes = require("./routes/task.routes");
-const projectRoutes = require("./routes/project.routes");
 
 const app = express();
 
@@ -14,7 +13,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/boards", boardRoutes);
 app.use("/api/tasks", taskRoutes);
-app.use("/api/projects", projectRoutes);
 
 app.get("/", (req, res) => {
     res.json({
