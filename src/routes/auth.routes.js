881b39ff22a169f3e2b78721dc12d4b7f492b4f4
Quốc Bảo <quocbaogg1407@gmail.com>
2026-07-23 19:58:27 +0700

    Thêm API Lấy thông tin user, đổi tên và đổi mật khẩu cho Settings

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
-const verifyToken = require("../middlewares/auth.middleware"); // Đường dẫn tới middleware xác thực token
+const verifyToken = require("../middleware/auth.middleware"); // Đường dẫn tới middleware xác thực token
 
 router.get("/test", (req, res) => {
     res.json({
